bf91c252b23521777d530a6a878ad1354ca426d7
SantoshKumar <Engr.santoshkumar@gmail.com>
2020-10-21 22:18:43 -0700

    New Program

--- src/Apple_FB_Prep/_oA_14_Shortest_Distance_from_All_Buildings.java
+++ src/Apple_FB_Prep/_oA_14_Shortest_Distance_from_All_Buildings.java
@@ -1,5 +1,4 @@
-/**
- * [ oA_14] [ Find Shortest_Distance_from_All_Buildings ]
+/* [ oA_14] [ Find Shortest_Distance_from_All_Buildings ]
  ____________________________________________________________________________________________________________
  You want to build a house on an empty land which reaches all buildings in the shortest amount of distance.
  You can only move up, down, left and right. You are given a 2D grid of values 0, 1 or 2, where:
