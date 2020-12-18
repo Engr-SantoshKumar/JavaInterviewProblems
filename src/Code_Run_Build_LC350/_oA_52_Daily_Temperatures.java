5e92f148d6c992bbe1bbcd3950783e934af74b4c
SantoshKumar <Engr.santoshkumar@gmail.com>
2020-12-18 07:14:49 -0800

    New Program

---
+++ src/Code_Run_Build_LC350/_oA_52_Daily_Temperatures.java
@@ -0,0 +1,44 @@
+/* [ _oA_52 ] [ Daily Temperatures ]
+_______________________________________________________________________________
+Given a list of daily temperatures T, return a list such that, for each day in the input,
+tells you how many days you would have to wait until a warmer temperature.
+If there is no future day for which this is possible, put 0 instead.
+
+For example, given the list of temperatures T = [73, 74, 75, 71, 69, 72, 76, 73],
+your output should be [1, 1, 4, 2, 1, 1, 0, 0].
+
+*/
+package Code_Run_Build_LC350;
+
+import java.util.Arrays;
+import java.util.Stack;
+
+public class _oA_52_Daily_Temperatures {
+        /*
+        temperatures = {6,4,5,7}, the next greater element for 6 is 7.
+        So we will return the index difference of the next
+        greater element that is at index 0 --> 6 and at index 3-->7, so we will return 3 - 0 = 3 for 6 at index 0
+         */
+    public static int[] dailyTemperature(int[] temperature){
+        Stack<Integer> stack = new Stack<>();
+        // since we are handling indexes instead of the temperatures we can directly store result in result array
+        int[] result = new int[temperature.length];
+        for(int curIndex =0; curIndex<temperature.length; curIndex++){
+            //logic: check if the temperature at curIndex is bigger than the top of the
+            // stack index temperature temperature[stack.peek()] if yes than
+            // do the diff of index and so on in the while loop
+            while(!stack.isEmpty() && temperature[curIndex] > temperature[stack.peek()]){
+                result[stack.peek()] =  curIndex - stack.pop();
+            }
+            stack.push(curIndex);
+            }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] temperatures = new int[]{73, 74, 75, 71, 69, 72, 76, 73};
+        System.out.println(Arrays.toString(dailyTemperature(temperatures)));
+    }
+
+
+}
--- src/_01_Coderust/_03_Stack_Queues/_07_NextGreaterElement_Comparator.java
+++ src/_01_Coderust/_03_Stack_Queues/_07_NextGreaterElement_Comparator.java
@@ -36,40 +36,11 @@ public class _07_NextGreaterElement_Comparator {
         return result;
     }
 
-    public static int[] nextGreaterElement4mRight(int[] arr){
 
-        int size = arr.length;
-
-        // Initialize the next greatest element
-        int max_from_right =  arr[size-1];
-
-        // The next greatest element for the rightmost
-        // element is always -1
-        arr[size-1] = -1;
-
-        // Replace all other elements with the next greatest
-        for (int i = size-2; i >= 0; i--)
-        {
-            // Store the current element (needed later for
-            // updating the next greatest element)
-            int temp = arr[i];
-
-            // Replace current element with the next greatest
-            arr[i] = max_from_right;
-
-            // Update the greatest element, if needed
-            if(max_from_right < temp)
-                max_from_right = temp;
-        }
-
-        return arr;
-    }
 
     public static void main(String[] arg){
-        int[] arr = {4,6,3,2,8,1};
-
+        int[] arr = {4,6,7,3, 4,8,1};
         System.out.println(Arrays.toString(nextGreaterElement(arr)));
-        System.out.println(Arrays.toString(nextGreaterElement4mRight(arr)));
 
     }
 }
